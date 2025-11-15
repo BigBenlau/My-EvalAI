b61f3293e85dbfa1154d1c4556f08f8fd5b5df02
Ben Lao <benlau413@gmail.com>
2025-11-15 19:33:43 +0800

    update the relative path of

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -140,7 +140,7 @@ def _find_dataset_root(eval_script_dir: str) -> str:
     Resolve dataset root. By default we expect a folder named 'dataset'
     next to the evaluation_script directory. Allow override via env.
     """
-    default_ds = os.path.abspath(os.path.join(eval_script_dir, "..", "dataset"))
+    default_ds = os.path.abspath(os.path.join(eval_script_dir, "dataset"))
     return os.environ.get("EVAL_DATA_ROOT", default_ds)
 
 
